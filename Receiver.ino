fbec1d6a4c89f8ec8337e7e553eb3cd667aa8368
Dmitrij P <118051854+DmitrijP1402@users.noreply.github.com>
2023-06-19 08:55:22 +0200

    Update Receiver.ino

--- Receiver.ino
+++ Receiver.ino
@@ -5,26 +5,26 @@ filename:  "Receiver.ino"
 ********************************************/
 
 void setup() {
-  Serial.begin(9600);
-  
-  pinMode (2, INPUT);
-  pinMode (3, INPUT);
-  pinMode (4, INPUT);
-  pinMode (5, INPUT);
-  pinMode (6, INPUT);
-  pinMode (7, INPUT);
-  pinMode (8, INPUT);
-  pinMode (9, INPUT);
-  pinMode (10, INPUT);
+  Serial.begin(9600); // Start the Serial Monitor
+
+  pinMode(2, INPUT); // Set digital pins 2-10 as INPUT
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
+  pinMode(6, INPUT);
+  pinMode(7, INPUT);
+  pinMode(8, INPUT);
+  pinMode(9, INPUT);
+  pinMode(10, INPUT);
 }
 
 void loop() {
-  byte startbit = digitalRead(10); 
+  byte startbit = digitalRead(10); // Read the status of the start bit
 
   if (startbit == 1) {
-    byte receivedByte = 0;
+    byte receivedByte = 0; // Set start bit to 0
 
-    receivedByte |= digitalRead(9);
+    receivedByte |= digitalRead(9) << 0; // Store received bits in a byte
     receivedByte |= digitalRead(8) << 1;
     receivedByte |= digitalRead(7) << 2;
     receivedByte |= digitalRead(6) << 3;
@@ -33,9 +33,9 @@ void loop() {
     receivedByte |= digitalRead(3) << 6;
     receivedByte |= digitalRead(2) << 7;
 
-    char receivedChar = (char)receivedByte;
+    char receivedChar = (char)receivedByte; // Convert byte to ASCII
 
-    Serial.print(receivedChar);
-    delay(1000);
-  } 
+    Serial.print(receivedChar); // Output ASCII/Text
+    delay(100);
+  }
 }
